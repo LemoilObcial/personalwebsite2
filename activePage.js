ca2ac8bb5fff7cf598fd5479c694d848ea85257b
LemoilObcial <lemoilobcial2@gmail.com>
2024-11-02 15:19:50 +0800

    update activePage.js

--- activePage.js
+++ activePage.js
@@ -1,6 +1,6 @@
-const activePage = window.location.pathname;
+/*const activePage = window.location.pathname;
 const navLinks = document.querySelectorAll('nav a').forEach(link => {
   if (link.href.includes(`${activePage}`)) {
     link.classList.add('active');
   }
-})
+})*/
